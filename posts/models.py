3e8673fbca821e65b0775df79905dd848903c29b
Artur <artur.silicki@gmail.com$git config --global user.email artur.silicki@gmail.com>
2017-09-30 17:43:01 +0200

    blablabla

--- posts/models.py
+++ posts/models.py
@@ -9,6 +9,7 @@ from groups.models import  Group
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
+asdasdasasfasf
 
 class Post(models.Model):
     user = models.ForeignKey(User, related_name="posts")
